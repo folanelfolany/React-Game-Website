0dc9dc651f841c7a4709ea4d24fa3a0c9b8c4b5c
Nadder Ragi <93446325+folanelfolany@users.noreply.github.com>
2024-11-03 19:10:20 +0000

    Some dynamic list functions

--- game-react-app/src/components/ListGroup.tsx
+++ game-react-app/src/components/ListGroup.tsx
@@ -5,20 +5,26 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function ListGroup() {
   let items = ["Cairo", "Alexandria", "Giza", "Luxor"];
-  items= [];
-
+  // items = [];
 
   return (
     //Add key to each list item so that React can keep track of the items, item
     //itself is a unique identifier
     // Braces {} render anything dynamically
     // If true: "?" , otherwise: ":"
+    // If items.length is 0 first condition is true so &&  display "No items to display"
     <Fragment>
       <h1>Test List</h1>
-      {items.length === 0 ? <p>No items in this list</p> : null}
+      {items.length === 0 && <p>No items to display</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("clicked")}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
