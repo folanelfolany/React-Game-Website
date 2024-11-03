ce4934caa44d96703f0d4838a0e91352e7136661
Nadder Ragi <93446325+folanelfolany@users.noreply.github.com>
2024-11-03 18:48:50 +0000

    Dynamic List Items

--- game-react-app/src/components/ListGroup.tsx
+++ game-react-app/src/components/ListGroup.tsx
@@ -1,12 +1,27 @@
+import { Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+//Function to map list items to li element
+
 function ListGroup() {
+  let items = ["Cairo", "Alexandria", "Giza", "Luxor"];
+  items= [];
+
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    //Add key to each list item so that React can keep track of the items, item
+    //itself is a unique identifier
+    // Braces {} render anything dynamically
+    // If true: "?" , otherwise: ":"
+    <Fragment>
+      <h1>Test List</h1>
+      {items.length === 0 ? <p>No items in this list</p> : null}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
