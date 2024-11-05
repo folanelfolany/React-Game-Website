168ec1f69eb8765c208fc351fea71c54ae821e64
Nadder Ragi <93446325+folanelfolany@users.noreply.github.com>
2024-11-05 16:36:23 +0000

    Props for dynamic lists

--- game-react-app/src/App.tsx
+++ game-react-app/src/App.tsx
@@ -1,11 +1,15 @@
-import ListGroup from './components/ListGroup';
-import 'bootstrap/dist/css/bootstrap.css';
-
+import ListGroup from "./components/ListGroup";
+import "bootstrap/dist/css/bootstrap.css";
 
 //We want to create a div with our message component inside it.
 function App() {
+  let items = ["Cairo", "Alexandria", "Giza", "Luxor"];
 
-  return <div><ListGroup /></div>
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
 export default App;
--- game-react-app/src/components/ListGroup.tsx
+++ game-react-app/src/components/ListGroup.tsx
@@ -1,12 +1,29 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 //Function to map list items to li element
 
-function ListGroup() {
-  let items = ["Cairo", "Alexandria", "Giza", "Luxor"];
-  // items = [];
 
+// {items : [], heading: string}
+//This helps us make the lists dynamic, including the ehading and the list itself
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+//Give the listgroup prop paramters
+//Destructuring paramters to give access to items and headings
+function ListGroup({items, heading}: Props) {
+  // let selectedIndex = 0;
+  //We want to say that this variable can change its state, so we use useState
+  //This is a HOOK - a state hook
+  //-1 means nothing is selected
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //Variable to handle clicking, always keep logic outside of direct function
+  // The event is passed as a parameter here ()
+  //Specify type of event paramter, MouseEvent in this case
+  // EVENT HANDLER
   return (
     //Add key to each list item so that React can keep track of the items, item
     //itself is a unique identifier
@@ -14,14 +31,18 @@ function ListGroup() {
     // If true: "?" , otherwise: ":"
     // If items.length is 0 first condition is true so &&  display "No items to display"
     <Fragment>
-      <h1>Test List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items to display</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log("clicked")}
+            onClick={() => {setSelectedIndex(index);}}
           >
             {item}
           </li>
